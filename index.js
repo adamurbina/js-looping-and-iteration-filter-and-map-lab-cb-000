31e5a43663c89021d7901afa251e143b7ff8a72a
Adam Urbina <adamurbina@gmail.com>
2019-01-23 01:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function driversWithRevenueOver(drivers, revenue) {
 
 function driverNamesWithRevenueOver(drivers, revenue) {
 
-  matchedDrivers =  drivers.filter(function (driver) {return driver.revenue === revenue;});
+  matchedDrivers =  drivers.filter(function (driver) {return driver.revenue > revenue;});
 
   return matchedDrivers.map(driver => driver.name)
 }
